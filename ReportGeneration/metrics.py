0bfc1f7363ad658b523abe5cb449eecfd155adb8
Denis Musinguzi <denismusinguzi2511@gmail.com>
2025-02-14 23:15:23 +0100

    Added more documentation for the metrics functions

--- ReportGeneration/metrics.py
+++ ReportGeneration/metrics.py
@@ -3,16 +3,23 @@ import pandas as pd
 from PIL import Image
 
 import torch
+import torch.nn as nn
 
 from typing import Dict
 
-
+from transformers import AutoProcessor
 
 bleu_score = evaluate.load('bleu')
 meteor_score = evaluate.load('meteor')
 rouge_score = evaluate.load('rouge')
 
-def get_model_preds(example, model, processor, device): 
+def get_model_preds(
+        example: pd.Series, 
+        model: nn.Module, 
+        processor: AutoProcessor, 
+        device: torch.device
+        ) -> str:
+    """Generate predictions from the model for the given example"""
     question = "Generate a medical report for the X-ray image provided"
     image    = Image.open(example['image_path']).convert("RGB")
     question = "<image>" + "<bos>" + question
@@ -22,6 +29,7 @@ def get_model_preds(example, model, processor, device):
     return outputs
 
 def compute_metrics(examples: pd.DataFrame) -> Dict[str, float]:
+    """Compute the BLEU, METEOR and ROUGE scores for the model predictions"""
     bleu    = bleu_score.compute(references = [examples["report"].lower()], predictions=[examples["Model Predictions"].lower()])
     meteor  = meteor_score.compute(references = [examples["report"].lower()], predictions=[examples["Model Predictions"].lower()])
     rouge   = rouge_score.compute(references = [examples["report"].lower()], predictions=[examples["Model Predictions"].lower()])
